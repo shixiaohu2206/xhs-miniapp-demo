63b23993d26c90c8080a70f2133dd1ad3b64990a
qiyao <shixiaohu.sxh@alibabapictures.com>
2024-08-05 14:16:52 +0800

    feat: add demo

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,19 @@
+const usingComponents = {
+  "aaa-bbb": "../../aaa-bbb/index",
+};
+
+if (process.env.TARO_ENV === "xhs") {
+  usingComponents["aaa-bbb"] = "./aaa-bbb-xhs/index";
+}
+
+if (process.env.TARO_ENV === "tt") {
+  usingComponents["aaa-bbb"] = "../../aaa-bbb-tt/index";
+}
+if (process.env.TARO_ENV === "kwai") {
+  usingComponents["aaa-bbb"] = "../../aaa-bbb-kwai/index";
+}
+
+export default definePageConfig({
+  navigationBarTitleText: "首页",
+  usingComponents,
+});
